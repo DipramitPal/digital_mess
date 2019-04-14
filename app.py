06b977174cdc71b58f553aba0b00adeac49703ae
Dipramit Pal <dipramit@iitkgp.ac.in>
2019-04-14 17:30:06 +0530

    Added Date selection and menu obtain

--- app.py
+++ app.py
@@ -29,16 +29,42 @@ def student_login():
 	password = request.form['password']
 	verify_email = controller.verify_email({'rollnumber':rollnumber,'password':password, 'db':cursor})
 	print(verify_email)
-	if verify_email:
-		return "Sucess"
-	else:
-		return "Fail"
+	dates = controller.get_dates({'db':cursor})
+
+	return render_template('student_dates.html',verify_email=verify_email,dates=dates)
 
 
 
 @app.route('/admin')
 def admin_page():
 	return render_template('admin_login.html')
+
+
+@app.route('/feed_menu',methods=['POST'])
+def feed_menu():
+	args = request.get_json(force=True)
+	arg = {}
+	arg['food'] = args['calendar']
+	arg['db'] = cursor
+	feeder = controller.feed_menu(arg)
+	if feeder == 1:
+		return "Success"
+	else:
+		return feeder
+
+@app.route('/get_menu',methods=['POST'])
+def get_menu():
+	arg = {}
+	arg['date'] = request.form['dateselect']
+	# arg['date'] = '2019-04-14'
+	arg['db'] = cursor
+	food_menu = controller.get_menu(arg)
+	if type(food_menu) == str:
+		return food_menu
+	else:
+		print("asdad")
+		print(food_menu)
+		return render_template('student_dashboard.html',food_menu = food_menu)
 
 
 
--- bin/controllers/controller.py
+++ bin/controllers/controller.py
@@ -1,5 +1,6 @@
 from bin.models import model
 from flask import jsonify
+import datetime
 
 def verify_email(arg):
 	try:
@@ -11,6 +12,49 @@ def verify_email(arg):
 			if result is None:
 				return 0
 			else:
-				return 1
+				return result[0]
+	except Exception as e:
+		return str(e)
+
+
+def feed_menu(args):
+	try:
+		
+		
+		for item in args['food']:
+			date = item['date']
+			food_string = ""
+			for food in item['items']:
+				print(food)
+				food_string = food_string + str(food) + ", "
+
+			query = args['db'].execute("INSERT INTO menu_calendar (date_of_serve,food_items) VALUES (%s,%s)",(str(date),str(food_string[:-2])))
+		return 1
+	except Exception as e:
+		return str(e)
+
+def get_dates(arg):
+	try:
+		dates = []
+		query = arg['db'].execute("SELECT date_of_serve FROM menu_calendar")
+		result = arg['db'].fetchall()
+		for date in result:
+			print(date[0])
+			dates.append(datetime.date.isoformat(date[0]))
+		return dates
+	except Exception as e:
+		return str(e)
+
+def get_menu(arg):
+	try:
+		date = arg['date']
+		query = arg['db'].execute("SELECT food_items FROM menu_calendar WHERE date_of_serve = %s",(str(date)))
+		result = arg['db'].fetchone()
+		print("here")
+		if result is None:
+			return "Fail"
+		else:
+			food_items = result[0].split(", ")
+			return food_items
 	except Exception as e:
 		return str(e)
